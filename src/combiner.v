3f2c88b6b535e3c426c8f68bbeb7af1f1d950220
Ryan Lund <ryan.lund@berkeley.edu>
2020-10-13 19:29:15 -0700

    Combiner intial attempt, testing needed

--- src/combiner.v
+++ src/combiner.v
@@ -7,16 +7,33 @@ module #(
   input rst,
   input en,
   input [1:0] cfg,                          // Single, Dual or Quad
-  input [2*MIN_WIDTH-1:0] partial_one,      
-  input [2*MIN_WIDTH-1:0] partial_two,
-  input [2*MIN_WIDTH-1:0] partial_three,
-  input [2*MIN_WIDTH-1:0] partial_four,
+  input [2*MIN_WIDTH-1:0] partial0,      
+  input [2*MIN_WIDTH-1:0] partial1,
+  input [2*MIN_WIDTH-1:0] partial2,
+  input [2*MIN_WIDTH-1:0] partial3,
 
-  output [4*MIN_WIDTH-1:0] out_one,         // Output only used for dual or quad configuration
-  output [4*MIN_WIDTH-1:0] out_two
+  output reg [2*MIN_WIDTH-1:0] out0,         // Output passed through in single mode
+  output reg [2*MIN_WIDTH-1:0] out1,         // Output split across one+two, three+four in dual mode
+  output reg [2*MIN_WIDTH-1:0] out2,       // Output split across all in quad mode
+  output reg [2*MIN_WIDTH-1:0] out3,
 );
 
-// Block is used to combine and accumulate partial products for dual and quad configurations
-// while conserving wire area and complexity 
+always @(*) begin
+  case (cfg)
+    `DUAL:
+      {out1, out0} = partial0 + (partial1 << MIN_WIDTH);
+      {out3, out2} = partial3 + (partial2 << MIN_WIDTH);
+    `QUAD:
+      {out3, out2, out1, out0} = partial0 + (partial1 << MIN_WIDTH) + (partial2 << 2*MIN_WIDTH) + (partial3 << 3*MIN_WIDTH);
+    default: begin
+      out0 = partial0;
+      out1 = partial1;
+      out2 = partial2;
+      out3 = partial3;
+    end
+
+end
+
+
 
 endmodule
