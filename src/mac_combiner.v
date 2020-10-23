9984b0f6186f00348eccc2010959d27391fe849b
Ryan Lund <ryan.lund@berkeley.edu>
2020-10-23 08:38:25 -0700

    Start movement of accumulator from mac -> combiner

--- src/mac_combiner.v
+++ src/mac_combiner.v
@@ -4,35 +4,45 @@ module mac_combiner (
   input clk,
   input rst,
   input en,
-  input [1:0] cfg,                          // Single, Dual or Quad
+  input [`MAC_CONF_WIDTH-1:0] cfg,                          // Single, Dual or Quad
   input [`MAC_INT_WIDTH-1:0] partial0,      
   input [`MAC_INT_WIDTH-1:0] partial1,
   input [`MAC_INT_WIDTH-1:0] partial2,
   input [`MAC_INT_WIDTH-1:0] partial3,
 
-  output reg [`MAC_ACC_WIDTH-1:0] out0,         // Output passed through in single mode
-  output reg [`MAC_ACC_WIDTH-1:0] out1,         // Output split across one+two, three+four in dual mode
-  output reg [`MAC_ACC_WIDTH-1:0] out2,       // Output split across all in quad mode
-  output reg [`MAC_ACC_WIDTH-1:0] out3
+  output [`MAC_ACC_WIDTH-1:0] out0,         // Output passed through in single mode
+  output [`MAC_ACC_WIDTH-1:0] out1,         // Output split across one+two, three+four in dual mode
+  output [`MAC_ACC_WIDTH-1:0] out2,       // Output split across all in quad mode
+  output [`MAC_ACC_WIDTH-1:0] out3
 );
 
+reg [`MAC_ACC_WIDTH-1:0] mult_only_out0,         // Output passed through in single mode
+reg [`MAC_ACC_WIDTH-1:0] mult_only_out1,         // Output split across one+two, three+four in dual mode
+reg [`MAC_ACC_WIDTH-1:0] mult_only_out2,       // Output split across all in quad mode
+reg [`MAC_ACC_WIDTH-1:0] mult_only_out3
+
 always @(*) begin
-  case (cfg)
+  case (cfg[`MAC_CONF_WIDTH-2:0])
     `MAC_DUAL: begin
-      {out1, out0} = partial0 + (partial1 << `MAC_MIN_WIDTH);
-      {out3, out2} = partial2 + (partial3 << `MAC_MIN_WIDTH);
+      {mult_only_out1, mult_only_out0} = partial0 + (partial1 << `MAC_MIN_WIDTH);
+      {mult_only_out3, mult_only_out2} = partial2 + (partial3 << `MAC_MIN_WIDTH);
     end
     `MAC_QUAD: begin
-      //{out3, out2, out1, out0} = partial0 + (partial1 << `MAC_MIN_WIDTH) + (partial2 << 2*`MAC_MIN_WIDTH) + (partial3 << 3*`MAC_MIN_WIDTH);
-      {out3, out2, out1, out0} = partial0 + {partial1, {`MAC_MIN_WIDTH{1'b0}}} + {partial2, {2*`MAC_MIN_WIDTH{1'b0}}} + {partial3, {3*`MAC_MIN_WIDTH{1'b0}}};
+      //{mult_only_out3, mult_only_out2, mult_only_out1, mult_only_out0} = partial0 + (partial1 << `MAC_MIN_WIDTH) + (partial2 << 2*`MAC_MIN_WIDTH) + (partial3 << 3*`MAC_MIN_WIDTH);
+      {mult_only_out3, mult_only_out2, mult_only_out1, mult_only_out0} = partial0 + {partial1, {`MAC_MIN_WIDTH{1'b0}}} + {partial2, {2*`MAC_MIN_WIDTH{1'b0}}} + {partial3, {3*`MAC_MIN_WIDTH{1'b0}}};
     end
     default: begin
-      out0 = partial0;
-      out1 = partial1;
-      out2 = partial2;
-      out3 = partial3;
+      mult_only_out0 = partial0;
+      mult_only_out1 = partial1;
+      mult_only_out2 = partial2;
+      mult_only_out3 = partial3;
     end
   endcase
 end
+
+assign out0 = cfg[`MAC_CONF_WIDTH-1] ? acc_out0 : mult_only_out0;
+assign out1 = cfg[`MAC_CONF_WIDTH-1] ? acc_out1 : mult_only_out1;
+assign out2 = cfg[`MAC_CONF_WIDTH-1] ? acc_out2 : mult_only_out2;
+assign out3 = cfg[`MAC_CONF_WIDTH-1] ? acc_out3 : mult_only_out3;
 
 endmodule
