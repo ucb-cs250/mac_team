0adf0d28ecfe2d0fde7721623edea96b5f89b99b
Anson Tsai <yuancheng.tsai@berkeley.edu>
2020-10-23 11:12:54 -0700

    Stopped using separate accumulate block since extra carry logic would be required

--- src/mac_acc_block.v
+++ src/mac_acc_block.v
@@ -26,10 +26,10 @@ reg [`MAC_ACC_WIDTH-1:0] mult_only_out1_reg;
 reg [`MAC_ACC_WIDTH-1:0] mult_only_out2_reg;
 reg [`MAC_ACC_WIDTH-1:0] mult_only_out3_reg;
 
-wire [`MAC_ACC_WIDTH-1:0] acc_out0;
-wire [`MAC_ACC_WIDTH-1:0] acc_out1;
-wire [`MAC_ACC_WIDTH-1:0] acc_out2;
-wire [`MAC_ACC_WIDTH-1:0] acc_out3;
+reg [`MAC_ACC_WIDTH-1:0] acc_out0;
+reg [`MAC_ACC_WIDTH-1:0] acc_out1;
+reg [`MAC_ACC_WIDTH-1:0] acc_out2;
+reg [`MAC_ACC_WIDTH-1:0] acc_out3;
 
 // Pipelining multiply-only results
 always @(posedge clk) begin
@@ -57,46 +57,32 @@ always @(*) begin
   endcase
 end
 
-// Accumulators (output is pipelined)
-accumulate acc_block_0
-(
-  .clk(clk), 
-  .reset(reset), 
-  .en(en), 
-  .init_val(cfg[`MAC_ACC_WIDTH-1:`MAC_CONF_WIDTH]), 
-  .din(mult_only_out0), 
-  .acc(acc_out0)
-);
-
-accumulate acc_block_1
-(
-  .clk(clk), 
-  .reset(reset), 
-  .en(en), 
-  .init_val(cfg[`MAC_ACC_WIDTH*2-1:`MAC_ACC_WIDTH+`MAC_CONF_WIDTH]), 
-  .din(mult_only_out1), 
-  .acc(acc_out1)
-);
-
-accumulate acc_block_2
-(
-  .clk(clk), 
-  .reset(reset), 
-  .en(en), 
-  .init_val(cfg[`MAC_ACC_WIDTH*3-1:`MAC_ACC_WIDTH*2+`MAC_CONF_WIDTH]), 
-  .din(mult_only_out2), 
-  .acc(acc_out2)
-);
-
-accumulate acc_block_3
-(
-  .clk(clk), 
-  .reset(reset), 
-  .en(en), 
-  .init_val(cfg[`MAC_ACC_WIDTH*4-1:`MAC_ACC_WIDTH*3+`MAC_CONF_WIDTH]), 
-  .din(mult_only_out3), 
-  .acc(acc_out3)
-);
+// Accumulators
+always @(posedge clk) begin
+  if (1'b0) begin // Bug where reset is high during start of test
+    acc_out0 = cfg[`MAC_ACC_WIDTH*1-1:`MAC_ACC_WIDTH*0+`MAC_CONF_WIDTH];
+    acc_out1 = cfg[`MAC_ACC_WIDTH*2-1:`MAC_ACC_WIDTH*1+`MAC_CONF_WIDTH];
+    acc_out2 = cfg[`MAC_ACC_WIDTH*3-1:`MAC_ACC_WIDTH*2+`MAC_CONF_WIDTH];
+    acc_out3 = cfg[`MAC_ACC_WIDTH*4-1:`MAC_ACC_WIDTH*3+`MAC_CONF_WIDTH];
+  end else begin
+    case (cfg[`MAC_CONF_WIDTH-2:0])
+      `MAC_DUAL: begin
+        {acc_out1, acc_out0} <= {acc_out1, acc_out0} + {mult_only_out1, mult_only_out0};
+        {acc_out3, acc_out2} <= {acc_out3, acc_out2} + {mult_only_out3, mult_only_out2};
+      end
+      `MAC_QUAD: begin
+        {acc_out3, acc_out2, acc_out1, acc_out0} <= {acc_out3, acc_out2, acc_out1, acc_out0} 
+          + {mult_only_out3, mult_only_out2, mult_only_out1, mult_only_out0};
+      end
+      default: begin
+        acc_out0 <= acc_out0 + mult_only_out0;
+        acc_out1 <= acc_out1 + mult_only_out1;
+        acc_out2 <= acc_out2 + mult_only_out2;
+        acc_out3 <= acc_out3 + mult_only_out3;
+      end
+    endcase
+  end
+end
 
 // Assigning outputs
 assign out0 = cfg[`MAC_CONF_WIDTH-1] ? acc_out0 : mult_only_out0_reg;
