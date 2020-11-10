a28523dd3518673b3277f9b74a529f0224b8c469
Ryan Lund <ryan.lund@berkeley.edu>
2020-11-10 15:32:55 -0800

    Add library for arithmetic cells

---
+++ arithmetic_cells/n_bit_adder.v
@@ -0,0 +1,30 @@
+module n_bit_adder #(
+	parameter N=8
+)(
+	input [N-1:0] 	A,
+	input [N-1:0] 	B,
+	input 			cin,
+	output [N-1:0]	SUM,
+	output			cout
+);
+
+wire [N:0] carry_bus;
+
+assign carry_bus[0] = cin;
+
+generate
+	genvar i;
+	for (i = 0; i < N; i = i + 1) begin: adder_chain
+		sky130_fd_sc_hd__fa adder (
+    		COUT(carry_bus[i+1]),
+    		SUM(SUM[i]),
+    		A(A[i]),
+    		B(B[i]),
+    		CIN(carry_bus[i]),
+		);
+	end
+endgenerate
+
+assign cout = cout_bus[N];
+
+endmodule
