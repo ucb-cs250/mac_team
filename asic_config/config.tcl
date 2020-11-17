b5424678ec341be4df3e3f6a1f9cafd3d75f59a0
Arya Reais-Parsi <himself@aryaparsi.com>
2020-11-16 19:34:40 -0800

    does this work?

--- asic_config/config.tcl
+++ asic_config/config.tcl
@@ -6,15 +6,18 @@
 set ::env(DESIGN_NAME) mac_cluster
 set ::env(PDK_VARIANT) sky130_fd_sc_hd
 
-set src_dir $::env(OPENLANE_ROOT)/designs/250/mac_team/src
+set mac_dir $::env(OPENLANE_ROOT)/designs/250/mac_team
+set src_dir $mac_dir/src
+set cells_dir $mac_dir/arithmetic_cells/src
 
 # Design config
 set ::env(CLOCK_PERIOD) 10
-set ::env(VERILOG_FILES) [glob $src_dir/*.v]
+set ::env(VERILOG_FILES) [concat [glob $src_dir/*.v] [glob $cells_dir/*.v]]
 set ::env(CLOCK_PORT) "clk"
 set ::env(CLOCK_NET) $::env(CLOCK_PORT)
 
 set filename $::env(DESIGN_DIR)/$::env(PDK)_$::env(STD_CELL_LIBRARY)_config.tcl
+puts $filename
 if { [file exists $filename] == 1} {
 	source $filename
 }
@@ -22,10 +25,12 @@ if { [file exists $filename] == 1} {
 # Synthesis config
 set ::env(SYNTH_STRATEGY) 2  ;# 1 fails
 
+set ::env(SYNTH_READ_BLACKBOX_LIB) 1
+
 set ::env(FP_SIZING) absolute
 # I think this goes LL_X LL_Y UR_X UR_Y, where LL=lower left, UR=upper right
 # Units probably microns
-set ::env(DIE_AREA) [list 0 0 650 650]
+set ::env(DIE_AREA) [list 0 0 700 700]
 
 # Floorplan config
 #set ::env(FP_CORE_UTIL) 5
