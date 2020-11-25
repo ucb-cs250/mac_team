ff60c87c36a1e6f780af0027b3bae9f1378bf85a
TsaiAnson <ansonytsai@gmail.com>
2020-11-25 12:24:09 -0800

    Added v0.0 run reports

---
+++ run_reports/v0_0-25-11_20-13/config.tcl
@@ -0,0 +1,20 @@
+# User config
+set ::env(DESIGN_NAME) mac_cluster
+
+# Change if needed
+set ::env(VERILOG_FILES) [glob $::env(DESIGN_DIR)/src/*.v]
+set ::env(SYNTH_READ_BLACKBOX_LIB) 1
+
+# Fill this
+set ::env(CLOCK_PERIOD) "250"
+set ::env(CLOCK_PORT) "clk"
+
+set ::env(SYNTH_MAX_FANOUT) 6
+set ::env(FP_CORE_UTIL) 35
+set ::env(PL_TARGET_DENSITY) 0.40
+
+set filename $::env(DESIGN_DIR)/$::env(PDK)_$::env(STD_CELL_LIBRARY)_config.tcl
+if { [file exists $filename] == 1} {
+	source $filename
+}
+
