ad50c562a1e9b7cfc9cc42e2aa5ecb776ac9e187
Arya Reais-Parsi <himself@aryaparsi.com>
2020-11-28 23:29:21 -0800

    undelete changes for arithmetic cells

--- asic_config/config.tcl
+++ asic_config/config.tcl
@@ -6,11 +6,13 @@
 set ::env(DESIGN_NAME) mac_cluster
 set ::env(PDK_VARIANT) sky130_fd_sc_hd
 
-set src_dir $::env(OPENLANE_ROOT)/designs/250/mac_team/src
+set mac_dir $::env(OPENLANE_ROOT)/designs/250/mac_team
+set src_dir $mac_dir/src
+set cells_dir $mac_dir/arithmetic_cells/src
 
 # Design config
 set ::env(CLOCK_PERIOD) 10
-set ::env(VERILOG_FILES) [glob $src_dir/*.v]
+set ::env(VERILOG_FILES) [concat [glob $src_dir/*.v] [glob $cells_dir/*.v]]
 set ::env(CLOCK_PORT) "clk"
 set ::env(CLOCK_NET) $::env(CLOCK_PORT)
 
@@ -22,10 +24,12 @@ if { [file exists $filename] == 1} {
 # Synthesis config
 set ::env(SYNTH_STRATEGY) 2  ;# 1 fails
 
+set ::env(SYNTH_READ_BLACKBOX_LIB) 1
+
 set ::env(FP_SIZING) absolute
 # I think this goes LL_X LL_Y UR_X UR_Y, where LL=lower left, UR=upper right
 # Units probably microns
-set ::env(DIE_AREA) [list 0 0 750 570]
+set ::env(DIE_AREA) [list 0 0 700 700]
 
 #set ::env(FP_CORE_UTIL) 5
 set ::env(PL_TARGET_DENSITY) 0.3
@@ -49,3 +53,9 @@ set ::env(GLB_RT_MAXLAYER) 5
 set ::env(ROUTING_CORES) 4
 
 set ::env(PDN_CFG) $::env(DESIGN_DIR)/pdn.tcl
+
+#set ::env(CLOCK_TREE_SYNTH) 0
+#set ::env(SYNTH_BUFFERING) 0
+#set ::env(SYNTH_SIZING) 0
+#set ::env(CELL_PAD) 8
+#set ::env(SYNTH_MAX_FANOUT) 6
