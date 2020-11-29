4f20c6b59b2247e93ff5a445fffb1f3358a41521
Arya Reais-Parsi <himself@aryaparsi.com>
2020-11-28 23:24:55 -0800

    add arya's working openlane config

--- asic_config/config.tcl
+++ asic_config/config.tcl
@@ -6,18 +6,15 @@
 set ::env(DESIGN_NAME) mac_cluster
 set ::env(PDK_VARIANT) sky130_fd_sc_hd
 
-set mac_dir $::env(OPENLANE_ROOT)/designs/250/mac_team
-set src_dir $mac_dir/src
-set cells_dir $mac_dir/arithmetic_cells/src
+set src_dir $::env(OPENLANE_ROOT)/designs/250/mac_team/src
 
 # Design config
 set ::env(CLOCK_PERIOD) 10
-set ::env(VERILOG_FILES) [concat [glob $src_dir/*.v] [glob $cells_dir/*.v]]
+set ::env(VERILOG_FILES) [glob $src_dir/*.v]
 set ::env(CLOCK_PORT) "clk"
 set ::env(CLOCK_NET) $::env(CLOCK_PORT)
 
 set filename $::env(DESIGN_DIR)/$::env(PDK)_$::env(STD_CELL_LIBRARY)_config.tcl
-puts $filename
 if { [file exists $filename] == 1} {
 	source $filename
 }
@@ -25,31 +22,30 @@ if { [file exists $filename] == 1} {
 # Synthesis config
 set ::env(SYNTH_STRATEGY) 2  ;# 1 fails
 
-set ::env(SYNTH_READ_BLACKBOX_LIB) 1
-
 set ::env(FP_SIZING) absolute
 # I think this goes LL_X LL_Y UR_X UR_Y, where LL=lower left, UR=upper right
 # Units probably microns
-set ::env(DIE_AREA) [list 0 0 700 700]
+set ::env(DIE_AREA) [list 0 0 750 570]
 
-# Floorplan config
 #set ::env(FP_CORE_UTIL) 5
-# Placement config
 set ::env(PL_TARGET_DENSITY) 0.3
 
-# CTS config
-# Routing config
 #set ::env(ROUTING_STRATEGY) 14 ;# run TritonRoute14
 #set ::env(GLB_RT_ADJUSTMENT) 0
-# Flow control config
+set ::env(GLB_RT_MAXLAYER) 5
+
+# add_macro_obs \
+#	-defFile $::env(CURRENT_DEF) \
+#	-lefFile $::env(MERGED_LEF_UNPADDED) \
+#	-obstruction core_obs \
+#	-placementX 500 \
+#	-placementY 500 \
+#	-sizeWidth 2200 \
+#	-sizeHeight 4300 \
+#	-fixed 1 \
+#	-layerNames "met5"
 
 # # threads for supporting tools
 set ::env(ROUTING_CORES) 4
 
 set ::env(PDN_CFG) $::env(DESIGN_DIR)/pdn.tcl
-
-#set ::env(CLOCK_TREE_SYNTH) 0
-#set ::env(SYNTH_BUFFERING) 0
-#set ::env(SYNTH_SIZING) 0
-#set ::env(CELL_PAD) 8
-#set ::env(SYNTH_MAX_FANOUT) 6
