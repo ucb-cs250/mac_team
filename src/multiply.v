f0bed2442456a08ecc0bb19d27ac270aca4e8792
Ryan Lund <ryan.lund@berkeley.edu>
2020-12-07 19:05:50 -0800

    Update multiplier to use array multiplier when MIN_WIDTH is not 8

--- src/multiply.v
+++ src/multiply.v
@@ -12,10 +12,19 @@ module multiply #(
 );
 
 // Separate file in case we want to modify how we do multiply...
-dadda_8_8_mult array_multiplier (
-	.A(A),
-	.B(B),
-	.C(C)
-);
-
+if (MAC_MIN_WIDTH == 8) begin
+	dadda_8_8_mult array_multiplier (
+		.A(A),
+		.B(B),
+		.C(C)
+	);
+end else begin
+	n_bit_array_multiplier #(
+		.N(MAC_MIN_WIDTH)
+	) array_multiplier (
+		.A(A),
+		.B(B),
+		.PROD(C)
+	);
+end
 endmodule
